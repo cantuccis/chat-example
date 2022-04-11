6772e56422fd2d6a195785dffc2fd50348fb6d20
Facundo Arancet Iza <facuarancet97@gmail.com>
2022-04-10 23:23:52 -0300

    New empty test project

---
+++ ChatExample/BusinessLogic.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
